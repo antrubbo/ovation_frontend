06e6a6c4c2c471ac3fd6de6edb29a2c06d622428
antrubbo <anthony.rubbo@gmail.com>
2021-01-27 12:08:27 -0500

    re-commit to get login and signup

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,51 @@
+import {useState} from "react"
+import { useHistory } from "react-router-dom"
+
+function LoginPage ({currentUser, setCurrentUser, email, setEmail, errors, setErrors, baseUrl}) {
+    const [fakePassword, setFakePassword] = useState("")
+    const history = useHistory()
+
+    function onLoginSubmit(evt) {
+        evt.preventDefault()
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email
+            })
+            })
+            .then((r) => r.json())
+            .then(user => {
+                console.log(user)
+                console.log(email)
+                if (user.errors) {
+                    setErrors(user.errors)
+                } else {
+                    setCurrentUser(user)
+                    setErrors("")
+                    history.push(`/user/${user.id}`)
+                }
+            });
+        setEmail("")
+        setFakePassword("")
+    }
+
+    return (
+        <>
+            {currentUser ? null : <h1>Log In</h1>}
+            <div>
+                {currentUser ? <h1>Welcome, {currentUser.name}</h1> : null}
+                {errors ? <p key={errors} style={{ color: 'red' }}>*{errors}</p> : null}
+                <form onSubmit={onLoginSubmit}>
+                    <input type="text" placeholder="Email Address.." value={email} onChange={evt => setEmail(evt.target.value)}></input>
+                    <input type="text" placeholder="Password.." value={fakePassword} onChange={evt => setFakePassword(evt.target.value)}></input>
+                    <input type="submit" value="Log In"></input>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage
---
+++ src/components/pages/SignupPage.js
@@ -0,0 +1,57 @@
+import {useState} from "react"
+import { useHistory } from "react-router-dom"
+
+function SignupPage({errors, setErrors, currentUser, setCurrentUser, baseUrl, name, email, setName, setEmail}) {
+    const [picture, setPicture] = useState("")
+    const history = useHistory()
+    
+    const formData = {
+        name,
+        email,
+        picture: picture ? picture : "https://www.thebalancecareers.com/thmb/p8dJbwwp5yyEzYA5SvuarUkZzRU=/1500x844/smart/filters:no_upscale()/GettyImages-639920934-5a0ce0d40d327a003650bbf4-5c09914f46e0fb00017183bb.jpg"
+    }
+
+    function setUserStateToQuotes() {
+        setName("")
+        setEmail("")
+        setPicture("")
+    }
+
+    function onFormSubmit(evt) {
+        evt.preventDefault()
+        fetch(`${baseUrl}/users`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+          })
+          .then(resp => resp.json())
+          .then(user => {
+            if(user.errors) {
+              setErrors(user.errors)
+              setUserStateToQuotes()
+            } else {
+              setCurrentUser(user)
+              setErrors("")
+              history.push(`/user/${user.id}`)
+            }
+          })
+        setUserStateToQuotes()
+    }
+
+    return (
+        <div>
+            {currentUser ? <h1>You've been successfully signed up, {currentUser.name}!</h1> : <h1>Sign Up</h1>}
+            {errors !== "" ? errors.map(error => <p key={error} style={{ color: 'red' }}>*{error}</p>) : null}
+            <form onSubmit={onFormSubmit} >
+                <input type="text" placeholder="Name.." value={name} onChange={evt => setName(evt.target.value)}></input>
+                <input type="text" placeholder="Email Address.." value={email} onChange={evt => setEmail(evt.target.value)}></input>
+                <input type="text" placeholder="Picture Url.." value={picture} onChange={evt => setPicture(evt.target.value)}></input>
+                <input type="submit"></input>
+            </form>
+        </div>
+    )
+}
+
+export default SignupPage
